273ea3cace1444f875136750ff7f70214f013fd5
DESKTOP-LN9PSIC\82102 <ejwuud2@naver.com>
2024-03-22 14:50:34 +0900

    First

---
+++ app/src/main/java/kr/ac/duksung/eyesone/MainActivity.java
@@ -0,0 +1,48 @@
+package kr.ac.duksung.eyesone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button nevigation;
+    Button detect;
+    Button parent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nevigation = findViewById(R.id.nevigation);
+        detect = findViewById(R.id.detect);
+        parent = findViewById(R.id.parent);
+
+        detect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BarrierActivity.class);
+                startActivity(intent);
+            }
+        });
+        nevigation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NeviActivity.class);
+                startActivity(intent);
+            }
+        });
+        parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), InfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/kr/ac/duksung/eyesone/NeviActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.duksung.eyesone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NeviActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nevi);
+    }
+}

b54b20a800052c638dadd77ec1a3a4369f4c089d
DESKTOP-LN9PSIC\82102 <ejwuud2@naver.com>
2024-04-17 00:59:18 +0900

    필요 없는 기능 삭제, 오류 잡기

--- app/src/main/java/kr/ac/duksung/eyesone/NeviActivity2.java
+++ app/src/main/java/kr/ac/duksung/eyesone/NeviActivity2.java
@@ -1,13 +1,14 @@
 package kr.ac.duksung.eyesone;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class TextActivity extends AppCompatActivity {
+public class NeviActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_text_activity);
+        setContentView(R.layout.activity_nevi2);
     }
 }

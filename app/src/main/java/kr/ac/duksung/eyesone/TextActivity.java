da42842255c6d493eb32656d1c87227b2924d01c
violet <101194343+joococo@users.noreply.github.com>
2024-04-21 18:32:28 +0900

    OCR & Camera

--- app/src/main/java/kr/ac/duksung/eyesone/TextActivity.java
+++ app/src/main/java/kr/ac/duksung/eyesone/TextActivity.java
@@ -1,29 +1,22 @@
 package kr.ac.duksung.eyesone;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.widget.TextView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import android.widget.Toast;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
@@ -31,13 +24,10 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+
 public class TextActivity extends AppCompatActivity {
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
-    private static final int PERMISSION_REQUEST_CAMERA = 2;
     private TextView textView;
-    private MyTTS tts;
-    private String url = "http://192.168.0.148:5000/upload"; // 서버 URL
-    private static final String TAG = "CameraDemo";
+    MyTTS tts;
 
 
     @Override
@@ -48,104 +38,62 @@ public class TextActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView);
         tts = new MyTTS(this, null);
 
-        Button captureButton = findViewById(R.id.capture_button);
-        captureButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(TextActivity.this, Manifest.permission.CAMERA)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(TextActivity.this,
-                            new String[]{Manifest.permission.CAMERA},
-                            PERMISSION_REQUEST_CAMERA);
-                } else {
-                    dispatchTakePictureIntent();
-                }
-            }
-        });
+        String url = "http://10.0.2.2:5000/upload";
+        //String url = "http://192.168.0.148:5000/upload";
+
+        new FetchDataTask().execute(url);
     }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } else {
-            Log.e(TAG, "No Activity found to handle the intent");
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_REQUEST_CAMERA) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                dispatchTakePictureIntent();
-            } else {
-                Toast.makeText(this, "Camera permission is necessary", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            new FetchDataTask().execute(imageBitmap);
-        }
-    }
-
-    private class FetchDataTask extends AsyncTask<Bitmap, Void, String> {
+    private class FetchDataTask extends AsyncTask<String, Void, String> {
         @Override
-        protected String doInBackground(Bitmap... bitmaps) {
+        protected String doInBackground(String... urls) {
             try {
-                Bitmap bitmap = bitmaps[0];
-                ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos);
-                byte[] imageData = bos.toByteArray();
-
-                OkHttpClient client = new OkHttpClient();
-                RequestBody requestBody = new MultipartBody.Builder()
-                        .setType(MultipartBody.FORM)
-                        .addFormDataPart("file", "image.jpg",
-                                RequestBody.create(MediaType.parse("image/jpeg"), imageData))
-                        .build();
-
-                Request request = new Request.Builder()
-                        .url(url)
-                        .post(requestBody)
-                        .build();
-
-                Response response = client.newCall(request).execute();
-                if (!response.isSuccessful()) {
-                    throw new IOException("Unexpected code " + response);
-                }
-                String jsonData = response.body().string();
+                System.out.println("Hello");
+                String jsonData = NetworkUtils.getServerData(urls[0]);
                 JSONObject jsonObject = new JSONObject(jsonData);
                 return jsonObject.getString("text");
             } catch (IOException | JSONException e) {
-                e.printStackTrace();
-                return "Failed to connect or process the image";
+                return null;
             }
         }
 
         @Override
         protected void onPostExecute(String result) {
+            System.out.println("Hello");
             super.onPostExecute(result);
-            if (result != null && !result.isEmpty()) {
-                textView.setText(result);
-                tts.speak(result);
-                Toast.makeText(TextActivity.this, "speak complete", Toast.LENGTH_SHORT).show();
+            if (result != null) {
+                // 유니코드 문자열을 정상적인 문자열로 변환하여 TextView에 표시
+                textView.setText(unescapeUnicode(result));
+                Toast.makeText(TextActivity.this, "speak start", Toast.LENGTH_SHORT).show();
+                tts.speak(unescapeUnicode(result).toString());
+                Toast.makeText(TextActivity.this, "speak Complete", Toast.LENGTH_SHORT).show();
+
             } else {
+                // 에러 처리
                 textView.setText("Error fetching data.");
-                Toast.makeText(TextActivity.this, "Failed to process the image", Toast.LENGTH_LONG).show();
             }
+        }
+
+        private String unescapeUnicode(String data) {
+            StringBuilder decoded = new StringBuilder();
+            for (int i = 0; i < data.length(); i++) {
+                char ch = data.charAt(i);
+                if (ch == '\\' && data.charAt(i + 1) == 'u') {
+                    String hexStr = data.substring(i + 2, i + 6);
+                    char unicodeChar = (char) Integer.parseInt(hexStr, 16);
+                    decoded.append(unicodeChar);
+                    i += 5; // 유니코드 이스케이프 시퀀스 넘김
+                } else {
+                    decoded.append(ch);
+                }
+            }
+            return decoded.toString();
         }
     }
 
-    @Override
     protected void onDestroy() {
         super.onDestroy();
         tts.destroy();
     }
+
 }

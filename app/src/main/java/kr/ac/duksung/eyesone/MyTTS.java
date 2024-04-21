101b4121e221d6918259a00d05815fe970606abd
violet <101194343+joococo@users.noreply.github.com>
2024-04-21 18:27:25 +0900

    OCR & Camera

---
+++ app/src/main/java/kr/ac/duksung/eyesone/MyTTS.java
@@ -0,0 +1,26 @@
+package kr.ac.duksung.eyesone;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+
+import java.util.Locale;
+
+public class MyTTS extends TextToSpeech {
+
+    public MyTTS(Context context, OnInitListener listener) {
+        super(context, listener);
+
+        this.setPitch(1.0f);    // 음성의 높낮이 설정
+        this.setSpeechRate(1.0f);    // 음성의 속도 설정
+        this.setLanguage(Locale.KOREA);
+    }
+
+    public void speak(CharSequence text) {
+        this.speak(text, TextToSpeech.QUEUE_FLUSH, null, "id1");
+    }
+
+    public void destroy() {
+        this.stop();
+        this.shutdown();
+    }
+}

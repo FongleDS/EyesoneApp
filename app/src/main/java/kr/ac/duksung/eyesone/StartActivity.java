268f975314f6842e5115a3189a0c5edcbc8bf5ab
idid10 <p0tlsi@duksung.ac.kr>
2024-04-21 22:45:52 +0900

    시작화면 추가

--- app/src/main/java/kr/ac/duksung/eyesone/StartActivity.java
+++ app/src/main/java/kr/ac/duksung/eyesone/StartActivity.java
@@ -12,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 import android.os.Handler;
 
 public class StartActivity extends AppCompatActivity {
+    //puch하려고 쓰는거
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

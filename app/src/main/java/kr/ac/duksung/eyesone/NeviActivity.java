d433eac87fe6ef1438111f8e63a11c7f275f1a65
idid10 <p0tlsi@duksung.ac.kr>
2024-04-21 20:40:25 +0900

    googlemap 나온다 이제

--- app/src/main/java/kr/ac/duksung/eyesone/NeviActivity.java
+++ app/src/main/java/kr/ac/duksung/eyesone/NeviActivity.java
@@ -1,14 +1,15 @@
 package kr.ac.duksung.eyesone;
-
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 
 public class NeviActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -19,9 +20,14 @@ public class NeviActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nevi);
 
+        // 권한 요청
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+        }
+
         // Get a handle to the fragment and register the callback.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
@@ -29,11 +35,13 @@ public class NeviActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Check if we were successful in obtaining the map.
+        if (mMap != null) {
+            // Add a marker in Sydney and move the camera
+            LatLng sydney = new LatLng(37, 127);
+            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Korea"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
     }
-
 
 }
